241a16088a3fd403776e71b0ec4568bf303bc038
Sohel <sohelhk0@gmail.com>
2022-08-25 20:49:00 +0600

    just a comment

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'ui/Spalshscreen.dart';
 
+
+// app main fuction
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
